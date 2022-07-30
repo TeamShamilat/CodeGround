6346d9743dd34509078d8f9af596c224968822c5
danyal afzal <danyalafzal243@gmail.com>
2022-07-30 23:08:12 +0500

    Multiplication

---
+++ CsharpPlay/Code/Danyal/Multiplication/Multiplication.D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPlay.Code.Danyal.Multiplicast;
+
+public partial class Multiplication
+{
+    static public int Sum(int first , int second)
+    {
+        return first * second;
+    }
+
+    [Theory]
+    [InlineData(3, 4, 12)]
+    [InlineData(4, 5, 20)]
+    [InlineData(5, 6, 30)]
+    [InlineData(6, 7, 42)]
+    [InlineData(10, 10, 100)]
+    [InlineData(5, 10, 50)]
+    [InlineData(2, 2, 4)]
+
+    public void FindSumtests(int first, int second, int demand)
+    {
+        int actual = Multiplication.Sum(first, second);
+        Assert.Equal(demand, actual);
+    }
+}
