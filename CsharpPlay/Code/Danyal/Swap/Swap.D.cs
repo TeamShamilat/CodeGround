0eb87d13e421972279e34fce9254d4f6091e601a
danyal afzal <danyalafzal243@gmail.com>
2022-08-03 15:20:29 +0500

    Incomplete 😂

---
+++ CsharpPlay/Code/Danyal/Swap/Swap.D.cs
@@ -0,0 +1,24 @@
+﻿///5.Write a program to swap two numbers. 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPlay.Code.Danyal.Swap;
+
+public partial class Swap
+{
+    public static void swap(int FirstNum, int SecondNum)
+    {
+        FirstNum = FirstNum + SecondNum;
+        SecondNum = FirstNum - SecondNum;
+        FirstNum = FirstNum - SecondNum;
+
+        
+
+    }  
+
+
+    }
+
