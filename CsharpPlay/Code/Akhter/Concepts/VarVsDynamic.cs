7e356ade8f7611fe50df4b12043893d794f444a7
Muhammad Asif <mhdaxif@gmail.com>
2022-08-05 20:16:58 +0500

    Update README.md

--- CsharpPlay/Code/Akhter/Concepts/VarVsDynamic.cs
+++ CsharpPlay/Code/Akhter/Concepts/VarVsDynamic.cs
@@ -9,49 +9,18 @@
  
 */
 
-namespace CsharpPlay.Akhtar.Concepts
+namespace CsharpPlay.Akhtar.Concepts;
+public partial class VarVsDynamic
 {
-    public partial class VarVsDynamic
+    [Fact]
+    public void Tests()
     {
-        dynamic Gpa = 0;   // dyanmic appear within class ,method etc.
-        // var  vGpa = 3.5;   // var may only appear within a local variable declaration
+       var userList = new List<dynamic> {
+        new List<string> {"20", "Asif", "Mardan"},
+        new List<int> {20, 50, 60, 80}
+       };
 
-        public dynamic GetGpa()
-        {
-            Gpa = 4.0;
-            // var marks = 33;        // It means if we assign the integer value at time of declaration then further we can not assign string, double etc. 
-            //  marks = "Pass";   // Cannot implicitly convert type 'string' to 'int'
-
-            Gpa = 3.5;    // In case of dynamic type there are no such types of restrictions.
-            return Gpa;
-        }
-
-        /*
-            We can’t create a method with var as return type
-            and also We can’t use the var keyword as a parameter for any method.
-
-        */
-        // public var SetGpa(var gpa){    
-        //     return gpa;
-        // }
-
-
-        public dynamic SetGpa(dynamic gpa)    //We can use dynamic type as parameter for method.Also as a return type is dynamic
-        {
-            return gpa;
-        }
+        Console.WriteLine(userList[0][1]);
     }
 
-    public partial class VarVsDynamicTest
-    {
-        [Theory]
-        [InlineData(3.5)]
-
-        public void Test(dynamic expected)
-        {
-            dynamic dGpa = new VarVsDynamic();
-            Assert.Equal(expected, dGpa.GetGpa());
-        }
-
-    }
 }
--- README.md
+++ README.md
@@ -2,3 +2,26 @@
  
 - Core programming concepts in C# language. 
 - Challenges
+
+
+##  GIT changes
+
+```bash
+# status
+$ git status 
+
+# fetch changes
+$ git fetch 
+
+# pull changes
+$ git pull
+
+# push changes
+$ git push
+
+## Reset hard
+$ git reset --hard
+
+## Reset soft
+$ git reset --soft 
+```
