54b55a1962c9394a0044a8221921b4e8cd7d0eeb
Muhsin <mohsinkhanswb@gmail.com>
2022-08-06 01:03:22 -0700

    update

--- CsharpPlay/Code/MohsinSwb/BasicProblems/Operations.cs
+++ CsharpPlay/Code/MohsinSwb/BasicProblems/Operations.cs
@@ -6,6 +6,22 @@ namespace CsharpPlay.MohsinSwb.BasicProblems
         {
             return one + two;
         }
+         public static int Sub(int one,int two)
+        {
+            return one - two;
+        }
+        public static int Mul(int one,int two)
+        {
+            return one * two;
+        }
+         public static int Div(int one,int two)
+        {
+            return one / two;
+        }
+        public static int Mod(int one,int two)
+        {
+            return one % two;
+        }
 
         [Theory]
         [InlineData (23,7,30)]
@@ -14,12 +30,6 @@ namespace CsharpPlay.MohsinSwb.BasicProblems
         {
            int actual = Operations.Add(one,two);
            Assert.Equal(expected,actual);
-        }
-        
-
-         public static int Sub(int one,int two)
-        {
-            return one - two;
         }
 
         [Theory]
@@ -30,6 +40,34 @@ namespace CsharpPlay.MohsinSwb.BasicProblems
            int actual = Operations.Sub(one,two);
            Assert.Equal(expected,actual);
         }
+
+        [Theory]
+        [InlineData (6,6,36)]
+
+        public void MulTest(int one,int two,int expected)
+        {
+           int actual = Operations.Mul(one,two);
+           Assert.Equal(expected,actual);
+        }
+
+        [Theory]
+        [InlineData (48,6,8)]
+
+        public void DivTest(int one,int two,int expected)
+        {
+           int actual = Operations.Div(one,two);
+           Assert.Equal(expected,actual);
+        }
+
+        [Theory]
+        [InlineData (31,3,1)]
+
+        public void ModTest(int one,int two,int expected)
+        {
+           int actual = Operations.Mod(one,two);
+           Assert.Equal(expected,actual);
+        }
+
 
     }
 }
